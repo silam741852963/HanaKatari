dd0d8919de401ba463138c52c17bf3398d43e07e
Ramu <nguyentrangsilam@gmail.com>
2024-02-04 20:11:19 +0700

    delete old file

--- hanakatari-next-app/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import { auth, firestore, storage } from "../lib/firebase";
-
-export default function Home() {
-  console.log(auth, firestore, storage);
-  return <main></main>;
-}
